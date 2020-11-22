ef8cd4ff380dc4610faf048626e1db38f5a566d5
MHB-1 <74874419+MHB-1@users.noreply.github.com>
2020-11-22 10:31:03 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var sword, fruit1, fruit2, fruit3, fruit4;
+var alein1, alein2;
+
+function preload(){
+  sword = setImage(sword.png);
+  fruit1 = setImage(fruit1.png);
+  fruit2 = setImage(fruit2.png);
+  fruit3 = setImage(fruit3.png);
+  fruit4 = setImage(fruit4.png);
+  alein1 = setImage(alein1.png);
+  alein2 = setImage(alein2.png);
+ 
+}
+
+function draw(){
+
+}
